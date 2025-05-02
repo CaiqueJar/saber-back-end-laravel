531ce7e7109049a991a60993636e4b1738650109
caique-webloper <caique@webloper.com.br>
2025-05-02 20:51:36 -0300

    fix: Trying to fix the cors problem #5

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,23 @@ use App\Http\Controllers\CategoriaRestauranteController;
 use App\Http\Controllers\RestauranteController;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\CategoriaProdutoController;
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/autenticar', [AutenticacaoController::class, 'login']);
+Route::middleware(CorsMiddleware::class)->group(function () {
 
-Route::get('/categorias', [CategoriaRestauranteController::class, 'list']);
-
-Route::resource('/restaurante', RestauranteController::class);
-Route::get('/restaurante/list/deleted', [RestauranteController::class, 'listTrashed']);
-Route::put('/restaurante/{id}/restore', [RestauranteController::class, 'restore']);
-Route::post('/restaurante/check/email-exists', [RestauranteController::class, 'checkIfEmailExists']);
-
-Route::resource('/categoria-produto', CategoriaProdutoController::class);
-
-Route::resource('/produto', ProdutoController::class);
+    Route::post('/autenticar', [AutenticacaoController::class, 'login']);
+    
+    Route::get('/categorias', [CategoriaRestauranteController::class, 'list']);
+    
+    Route::resource('/restaurante', RestauranteController::class);
+    Route::get('/restaurante/list/deleted', [RestauranteController::class, 'listTrashed']);
+    Route::put('/restaurante/{id}/restore', [RestauranteController::class, 'restore']);
+    Route::post('/restaurante/check/email-exists', [RestauranteController::class, 'checkIfEmailExists']);
+    
+    Route::resource('/categoria-produto', CategoriaProdutoController::class);
+    
+    Route::resource('/produto', ProdutoController::class);
+});
 
