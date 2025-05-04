460a30616c95486b0804479f4f4e03eadd1ba004
caique-webloper <caique@webloper.com.br>
2025-05-04 15:24:51 -0300

    feat: tentando salvar a imagem #4

--- app/Http/Requests/ProdutoRequest.php
+++ app/Http/Requests/ProdutoRequest.php
@@ -27,7 +27,6 @@ class ProdutoRequest extends FormRequest
             'descritivo' => 'nullable|string',
             'imagem' => 'required',
             'preco' => 'required|string|between:0,99999999.99',
-            'disponibilidade' => 'boolean',
             'desconto' => 'nullable|string|between:0,99999999.99',
             'status' => 'required|in:disponivel,indisponivel',
         ];
