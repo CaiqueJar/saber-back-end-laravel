ced9d45e817df108798e770cb48a32f8072227f2
caique-webloper <caique@webloper.com.br>
2025-05-25 12:20:27 -0300

    feat: limiting restaurant categories #2

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/categorias/{?limit}', [CategoriaRestauranteController::class, 'list']);
+Route::get('/categorias/{limit?}', [CategoriaRestauranteController::class, 'list']);
 
 /**
  * ENDPOINTS DO USUÁRIO
