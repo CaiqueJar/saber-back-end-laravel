35b05348c23479ee5dff254bb868b8767e0668de
caique-webloper <caique@webloper.com.br>
2025-06-02 18:52:58 -0300

    test: Mp test

---
+++ app/Http/Controllers/MpController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MpController extends Controller
+{
+    public function connectToMercadoPago()
+    {
+        $client_id = env('MP_CLIENT_ID');
+        $redirect_uri = route('mp.return');
+
+        $link = "https://auth.mercadopago.com.br/authorization?client_id={$client_id}&response_type=code&platform_id=mp&redirect_uri={$redirect_uri}";
+
+        return redirect($link);
+    }
+
+    public function redirect(Request $request)
+    {
+        info($request->all());
+    }
+
+    public function webhook(Request $request)
+    {
+        info($request->all());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,66 @@
 <?php
 
+use App\Http\Controllers\MpController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use MercadoPago\Client\Common\RequestOptions;
+use MercadoPago\Client\Order\OrderClient;
+use MercadoPago\Exceptions\MPApiException;
+use MercadoPago\MercadoPagoConfig;
 
+Route::view('/', 'pagamento');
+
+Route::post('/pagar', function(Request $request) {
+    MercadoPagoConfig::setAccessToken("APP_USR-3903069347230855-052508-880e5b48a23caa2dc1e4fadef469578c-387424373");
+    
+    $data = $request->all();
+    
+    try {
+        $paymentRequest = [
+            "transaction_amount" => (float)$data['transaction_amount'],
+            "token" => $data['token'],
+            "description" => $data['description'],
+            "installments" => (int)$data['installments'],
+            "payment_method_id" => $data['payment_method_id'],
+            "issuer_id" => $data['issuer_id'],
+            "payer" => [
+                "email" => $data['payer']['email'],
+                "identification" => [
+                    "type" => $data['payer']['identification']['type'],
+                    "number" => $data['payer']['identification']['number']
+                ]
+            ],
+            "binary_mode" => true
+        ];
+
+        $client = new \MercadoPago\Client\Payment\PaymentClient();
+        $payment = $client->create($paymentRequest);
+
+        return response()->json([
+            'status' => $payment->status,
+            'status_detail' => $payment->status_detail,
+            'id' => $payment->id
+        ]);
+        
+    } catch (MPApiException $e) {
+        $error = $e->getApiResponse()->getContent();
+        Log::error('Erro MP Detalhado:', [
+            'status' => $e->getApiResponse()->getStatusCode(),
+            'error' => $error,
+            'request' => $data
+        ]);
+        
+        return response()->json([
+            'error' => 'Pagamento falhou',
+            'status' => $e->getApiResponse()->getStatusCode(),
+            'details' => $error['cause'] ?? $error['message'] ?? $error
+        ], 400);
+    }
+})->name('pagar');
+
+
+Route::get('/mp-auth', [MpController::class, 'connectToMercadoPago']);
+Route::get('/mp-return', [MpController::class, 'redirect'])->name('mp.return');
+Route::get('/webhook', [MpController::class, 'webhook'])->name('mp.webhook');
